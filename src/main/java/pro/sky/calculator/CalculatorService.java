78d0629e2101e45a085d9e67fdf3b9d020939bc4
teafighter <32483346+teafighter@users.noreply.github.com>
2024-05-06 11:51:56 +0300

    Update CalculatorService.java
    changed methods to be non-static

--- src/main/java/pro/sky/calculator/CalculatorService.java
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -7,19 +7,19 @@ public class CalculatorService {
     public int num1;
     public int num2;
 
-    public static int addition(int num1, int num2) {
+    public int addition(int num1, int num2) {
         return num1 + num2;
     }
 
-    public static int subtraction(int num1, int num2) {
+    public int subtraction(int num1, int num2) {
         return num1 - num2;
     }
 
-    public static int multiplication(int num1, int num2) {
+    public int multiplication(int num1, int num2) {
         return num1 * num2;
     }
 
-    public static double division(int num1, int num2) {
+    public double division(int num1, int num2) {
         return (double) num1 / num2;
     }
 }
